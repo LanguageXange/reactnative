6181d0a9f5af0940f9973e04773a2ce8be9c750c
LanguageXange <hlin0017@seattlecentral.edu>
2021-02-03 21:56:14 -0800

    install eslint prettier react native paper

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+// yarn lint to run linting
+//console.log(StatusBar.currentHeight, "this will solve android issue as SafeAreaView works only on iOS");
 export default function App() {
+  const [query, setQuery] = useState("");
+
+  const onSearch = (input) => setQuery(input);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchBarContainer}>
+          <Searchbar
+            onChangeText={onSearch}
+            placeholder="Search ..."
+            value={query}
+          />
+
+          <Text>You are searching: {query}</Text>
+        </View>
+
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  searchBarContainer: {
+    backgroundColor: "#fb850b",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15,
+    fontSize: 20,
+  },
+  list: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    padding: 15,
   },
 });
